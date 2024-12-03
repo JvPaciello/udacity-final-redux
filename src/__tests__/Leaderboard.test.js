24d62a2266074bfbc29affcf46fe4581d5966f1f
João Victor de Aquino Netto Paciello <joaovictorpaciello@gmail.com>
2024-12-03 16:19:19 -0300

    Removed the login test and added the leaderboard test

---
+++ src/__tests__/Leaderboard.test.js
@@ -0,0 +1,49 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
+import Leaderboard from "../components/Leaderboard/Leaderboard";
+
+
+const mockStore = configureStore([]);
+const initialState = {
+  users: {
+    sarahedo: {
+      id: "sarahedo",
+      name: "Sarah Edo",
+      avatarURL: "https://austinmonthly.wppcdn.com/wp-content/uploads/2019/10/JackieVenson1.jpeg",
+      answers: {
+        "8xf0y6ziyjabvozdd253nd": "optionOne",
+        "6ni6ok3ym7mf1p33lnez": "optionOne",
+        "am8ehyc8byjqgar0jgpub9": "optionTwo",
+        "loxhs1bqm25b708cmbf3g": "optionTwo",
+      },
+      questions: ["8xf0y6ziyjabvozdd253nd", "am8ehyc8byjqgar0jgpub9"],
+    },
+    tylermcginnis: {
+      id: "tylermcginnis",
+      name: "Tyler McGinnis",
+      avatarURL: "https://blogs.correiobraziliense.com.br/trilhasonora/wp-content/uploads/sites/39/2020/10/CBNFOT081020100047-550x549.jpg",
+      answers: {
+        "vthrdm985a262al8qx3do": "optionOne",
+        "xj352vofupe1dqz9emx13r": "optionTwo",
+      },
+      questions: ["loxhs1bqm25b708cmbf3g", "vthrdm985a262al8qx3do"],
+    },
+  },
+};
+
+describe("Leaderboard Snapshot Test", () => {
+  it("renders correctly with given state", () => {
+    const store = mockStore(initialState);
+
+    const component = renderer.create(
+      <Provider store={store}>
+        <Leaderboard />
+      </Provider>
+    );
+
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
