eeda094e697d45f6c6b1bf67feae8f9a4ca2e231
João Victor de Aquino Netto Paciello <joaovictorpaciello@gmail.com>
2024-12-03 23:19:02 -0300

    These updates ensure that the user is correctly redirected to the page visited prior to logging in.

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,13 +1,12 @@
-import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { setAuthedUser } from '../../actions/authedUser';
-import UserSelect from '../UserSelect';
-import { useNavigate } from 'react-router-dom';
-import './Login.css';
-
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { setAuthedUser } from "../../actions/authedUser";
+import UserSelect from "../UserSelect";
+import { useNavigate } from "react-router-dom";
+import "./Login.css";
 
 function Login() {
-  const [selectedUser, setSelectedUser] = useState('');
+  const [selectedUser, setSelectedUser] = useState("");
   const users = useSelector((state) => state.users);
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -15,16 +14,20 @@ function Login() {
   const handleLogin = () => {
     if (selectedUser) {
       dispatch(setAuthedUser(selectedUser));
-      navigate('/dashboard')
+
+      const lastVisitedPath = localStorage.getItem("lastVisitedPath") || "/dashboard";
+      navigate(lastVisitedPath);
+
+      localStorage.removeItem("lastVisitedPath");
     } else {
-      alert('Please, select an user.');
+      alert("Please, select a user.");
     }
   };
 
   return (
     <div className="login-container">
       <h1>Login</h1>
-      <p>Select an user to continue:</p>
+      <p>Select a user to continue:</p>
       <UserSelect users={users} onSelect={setSelectedUser} />
       <button onClick={handleLogin}>Enter</button>
     </div>
--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,10 +1,17 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 
-function PrivateRoute({element}){
-    const authedUser = useSelector((state) => state.authedUser);
-    return authedUser ? element : <Navigate to = "/"/>;
+function PrivateRoute({ element }) {
+  const authedUser = useSelector((state) => state.authedUser);
+  const location = useLocation();
+
+  if (!authedUser) {
+    localStorage.setItem("lastVisitedPath", location.pathname);
+    return <Navigate to="/" />;
+  }
+
+  return element;
 }
 
 export default PrivateRoute;
